e9c9e44dbc9a3a437ce76714d9f4a8337638baf1
SagarDrizzle <sagar.drizzle@gmail.com>
2025-03-28 12:10:18 +0530

    1st commit on 25-03-2025 12:24

--- two sum.py
+++ two sum.py
@@ -1,1 +1,13 @@
-# new file on 10-03-2025 13:16
+#commit on 28 March 2025
+
+n= 10
+row_lenght = 1
+
+while n > 0 :
+    print("#" , end= " ")
+
+    for _ in range(row_lenght):
+        print(n, end=" ")
+        n = n-1
+row_lenght += 1
+
