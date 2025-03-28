01b1dc3c33e82ffc82dc1e83362f28517d2230c9
SagarDrizzle <sagar.drizzle@gmail.com>
2025-03-28 12:43:20 +0530

    2nd commit on 25-03-2025 12:43

--- two sum.py
+++ two sum.py
@@ -1,5 +1,5 @@
 #commit on 28 March 2025
-
+'''
 n= 10
 row_lenght = 1
 
@@ -10,4 +10,36 @@ while n > 0 :
         print(n, end=" ")
         n = n-1
 row_lenght += 1
+'''
+
+#  input input = "aabccddeee"  output = "2a1b2c2d3e"
+
+input = "aabccddeee"
+cur_char = input[0]
+count = 1
+output_list = [ ]
+n= len(input)
+for char in range (1 , n):
+    if input[char] == cur_char :
+        count = count +  1
+    else:
+        output_list.append(f"{count}{cur_char}")
+        count = 1
+        cur_char = input[char]
+output_list.append(f"{count}{cur_char}")
+output = ''.join(output_list)
+
+print(output)
+
+
+
+
+
+
+
+
+
+
+
+
 
